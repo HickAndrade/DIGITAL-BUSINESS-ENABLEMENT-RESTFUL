b09f6b06b4f77a4c3a2b4df279b33e8064271e5e
Logon Aluno <logonrmlocal@fiap.com.br>
2019-03-15 09:45:33 -0300

    Post

--- 02-LojaApp/src/br/com/fiap/bo/ProdutoBO.java
+++ 02-LojaApp/src/br/com/fiap/bo/ProdutoBO.java
@@ -6,27 +6,48 @@ import java.util.List;
 import br.com.fiap.to.ProdutoTO;
 
 public class ProdutoBO {
-	public static ProdutoTO consultaBO(int codProd) {
+	private static ProdutoBO produtoBo;
+	private static	ArrayList<ProdutoTO> lista = new ArrayList<ProdutoTO>();
+	
+	public ProdutoBO() {
+		lista.add(ProdutoBO.consultaBO(401));
+		lista.add(ProdutoBO.consultaBO(402));
+		lista.add(ProdutoBO.consultaBO(403));
+	}
 
+	public static ProdutoBO getInstance() {
+		if (produtoBo == null) {
+			produtoBo = new ProdutoBO();
+		}
+		
+		return produtoBo;
+	}
+	
+
+	public static ProdutoTO consultaBO(int codProd) {
+	
 		if (codProd == 401) {
 			return new ProdutoTO(codProd, 2.99, 4, "Camisa Branca");
 
 		} else if (codProd == 402) {
-			return new ProdutoTO(codProd, 2.99, 4, "Camisa Azul");
+		return new ProdutoTO(codProd, 2.99, 4, "Camisa Azul");
 		} else if (codProd == 403) {
 			return new ProdutoTO(codProd, 2.99, 4, "Camisa Rosa");
 		} else {
 			return new ProdutoTO(-1, 0, 0, "Produto não encontrado");
 		}
 	}
-
-	public static List<ProdutoTO> listar() {
-		ArrayList<ProdutoTO> lista = new ArrayList<ProdutoTO>();
-		lista.add(ProdutoBO.consultaBO(401));
-		lista.add(ProdutoBO.consultaBO(402));
-		lista.add(ProdutoBO.consultaBO(403));
-			
+	
+	
+	
+	
+	public static List<ProdutoTO> listar() {	
 		return lista;
 	}
 
+	public void cadastrar(ProdutoTO produto) {
+		lista.add(produto);
+	}
+	
+	
 }
--- 02-LojaApp/src/br/com/fiap/resource/ProdutoResource.java
+++ 02-LojaApp/src/br/com/fiap/resource/ProdutoResource.java
@@ -2,21 +2,53 @@ package br.com.fiap.resource;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import br.com.fiap.bo.ProdutoBO;
 import br.com.fiap.to.ProdutoTO;
 
+@SuppressWarnings("all")
 @Path("/Produto")
 public class ProdutoResource {
-	private ProdutoBO produtoBo = new ProdutoBO();
-	
+	private ProdutoBO produtoBo = ProdutoBO.getInstance();
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<ProdutoTO> buscar(){
+	public List<ProdutoTO> buscar() {
 		return produtoBo.listar();
 	}
+
+	@Path("/{id}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ProdutoTO buscar(@PathParam("id") int codigo) {
+
+		return ProdutoBO.consultaBO(codigo);
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response cadastrar(ProdutoTO produto, @Context UriInfo uriInfo) {
+		produtoBo.cadastrar(produto);
+		System.out.println(produto.getCodigo());
+		System.out.println(produto.getPreco());
+		System.out.println(produto.getQuantidade());
+		System.out.println(produto.getDescricao());
+		UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+		builder.path(Integer.toString(produto.getCodigo()));
+		return Response.created(builder.build()).build();
+		
+		
+	}
+
 }
